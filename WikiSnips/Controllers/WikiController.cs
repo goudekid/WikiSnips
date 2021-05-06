fbe1e31c254cd29742098d932f1b382d9eea0f61
goog <just pm me bro>
2021-05-05 22:41:40 -0700

    Add project files.

---
+++ WikiSnips/Controllers/WikiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using WikiSnips.Models;
+
+namespace WikiSnips.Controllers
+{
+    public class WikiController
+    {
+        IRestClient Client = new RestClient();
+
+        public async Task<ActionResult<string>> GetRandomWikiText()
+        {
+            IRestRequest request = new RestRequest("https://en.wikipedia.org/api/rest_v1/page/random/summary", Method.GET, DataFormat.Json);
+            IRestResponse<WikiArticleResponse> response = await Client.ExecuteAsync<WikiArticleResponse>(request);
+
+            return response.Content;
+        }
+    }
+}
---
+++ WikiSnips/Models/WikiArticleResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace WikiSnips.Models
+{
+    public class WikiArticleResponse
+    {
+        public string Displaytitle { get; set; }
+        public string Extract_html { get; set; }
+    }
+}
